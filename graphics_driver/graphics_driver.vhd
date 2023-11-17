04fe825a203f33066712deacb67fd5610cc35029
Ard Geuze <geuzeard@gmail.com>
2023-11-17 15:17:07 +0100

    Add tb

--- graphics_driver/graphics_driver.vhd
+++ graphics_driver/graphics_driver.vhd
@@ -5,8 +5,8 @@ use ieee.numeric_std.all;
 -- rgb output, 4 bits per color, hsync, vsync
 entity graphics_driver is
     port (
-        h_pos : in std_logic_vector(10 downto 0);
-        v_pos : in std_logic_vector(9 downto 0);
+        h_pos : in std_logic_vector(9 downto 0);
+        v_pos : in std_logic_vector(8 downto 0);
         red   : out std_logic_vector(3 downto 0);
         green : out std_logic_vector(3 downto 0);
         blue  : out std_logic_vector(3 downto 0)
