cb2448ebb4ac4637658b511beb5109f6d3456cad
Ard Geuze <geuzeard@gmail.com>
2023-11-17 16:40:12 +0100

    Remove big files

--- graphics_driver/pos_test_generator.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-f = open("./v_pos.txt", "a")
-
-
-for i in range(0, 480):
-    for j in range(0, 640):
-        f.write("\""+ "{0:b}".format(j).rjust(10, "0") + "\" after " + str(i * 25600 + j * 40) + " ns,\n")
