2377633edeb85cf87a4b4839f01e9a38bb3218c4
Ard Geuze <geuzeard@gmail.com>
2023-11-17 16:50:27 +0100

    Add boundaries for graphics driver

--- graphics_driver/graphics_driver.vhd
+++ graphics_driver/graphics_driver.vhd
@@ -19,14 +19,14 @@ architecture behavior of graphics_driver is
 begin
     process (h_pos, v_pos)
     begin
-        if (to_integer(unsigned(h_pos)) < 640) then
-            red   <= 2;
-            green <= 15;
-            blue  <= 3;
-        else
+        if (to_integer(unsigned(v_pos)) < 145 or to_integer(unsigned(v_pos)) > 784 or to_integer(unsigned(h_pos)) < 32 or to_integer(unsigned(h_pos)) > 511) then
             red   <= 0;
             green <= 0;
             blue  <= 0;
+        else
+            red   <= 2;
+            green <= 15;
+            blue  <= 3;
         end if;
     end process;
 end architecture;
