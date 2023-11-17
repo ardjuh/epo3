5f6e00a475818e852e686affa58670a4a6c37b3d
Fkrete <131753232+fkrete@users.noreply.github.com>
2023-11-17 14:15:54 +0100

    Update vga_driver.vhd
    base 1

--- vga_driver/vga_driver.vhd
+++ vga_driver/vga_driver.vhd
@@ -1,3 +1,12 @@
-l
-2
-hoi
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity graphics_driver is
+    port(clk   	: in std_logic;
+        reset 	: in std_logic;
+        H_sync	: out std_logic;
+        V_sync	: out std_logic;
+        R	: out std_logic;
+	G	: out std_logic;
+	B	: out std_logic);
+end graphics_driver;
