ec6ea5c27516f98cc3f37b1643a669280779aedc
Ard Geuze <geuzeard@gmail.com>
2023-11-17 16:41:34 +0100

    Fix signals

--- graphics_driver/graphics_driver_tb.vhd
+++ graphics_driver/graphics_driver_tb.vhd
@@ -14,9 +14,9 @@ architecture behaviour of graphics_driver_tb is
 
     signal h_pos : std_logic_vector(9 downto 0);
     signal v_pos : std_logic_vector(8 downto 0);
-    signal red   : (3 downto 0);
-    signal green : std_logic_vector(3 downto 0);
-    signal blue  : std_logic_vector(3 downto 0);
+    signal red   : integer range 0 to 15;
+    signal green : integer range 0 to 15;
+    signal blue  : integer range 0 to 15;
 begin
     test : graphics_driver port map(h_pos, v_pos, red, green, blue);
 end architecture;
