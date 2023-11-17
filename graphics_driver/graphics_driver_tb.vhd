9a4e495d61c74b634bd5245906a5ae57c45e9162
Slurpmeester <100770592+Slurpmeester@users.noreply.github.com>
2023-11-17 16:57:20 +0100

    Update graphics_driver_tb.vhd

--- graphics_driver/graphics_driver_tb.vhd
+++ graphics_driver/graphics_driver_tb.vhd
@@ -1,14 +1,14 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-
-architecture behaviour of graphics_driver_tb is
+use ieee.numeric_std.all;
+architecture behaviour of gpu_driver_tb is
     component graphics_driver
         port (
             h_pos : in std_logic_vector(9 downto 0);
             v_pos : in std_logic_vector(8 downto 0);
             red   : out integer range 0 to 15;
             green : out integer range 0 to 15;
-            blue  : out integer range 0 to 15;
+            blue  : out integer range 0 to 15
         );
     end component;
 
