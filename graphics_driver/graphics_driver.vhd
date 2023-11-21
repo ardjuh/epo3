9f8a8f66be0a331e875b865f480e4e787a6f3536
Ard Geuze <geuzeard@gmail.com>
2023-11-21 10:29:24 +0100

    Add boundaries back

--- graphics_driver/graphics_driver.vhd
+++ graphics_driver/graphics_driver.vhd
@@ -1,21 +1,32 @@
-library IEEE;
-use IEEE.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-architecture behaviour of gpu_driver is
+-- rgb output, 4 bits per color, hsync, vsync
+entity graphics_driver is
+    port (
+        h_pos : in std_logic_vector(9 downto 0);
+        v_pos : in std_logic_vector(8 downto 0);
+        red   : out integer range 0 to 15;
+        green : out integer range 0 to 15;
+        blue  : out integer range 0 to 15
+    );
+end entity graphics_driver;
+
+-- Begin with displaying half a blue screen in a process
+architecture behavior of graphics_driver is
 
 begin
     process (h_pos, v_pos)
     begin
-        if (to_integer(unsigned(h_pos)) < 640) then
-            red   <= 2;
-            green <= 15;
-            blue  <= 3;
-        else
+        if (to_integer(unsigned(v_pos)) < 145 or to_integer(unsigned(v_pos)) > 784 or to_integer(unsigned(h_pos)) < 32 or to_integer(unsigned(h_pos)) > 511) then
             red   <= 0;
             green <= 0;
             blue  <= 0;
+        else
+            red   <= 2;
+            green <= 15;
+            blue  <= 3;
         end if;
     end process;
-
-end behaviour;
+end architecture;
