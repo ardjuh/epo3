2af4197ced96c4f6ac6db9191443e302b572d4a5
Ard Geuze <geuzeard@gmail.com>
2023-11-21 11:20:03 +0100

    Add one card

--- graphics_driver/gpu_driver.vhd
+++ graphics_driver/gpu_driver.vhd
@@ -4,18 +4,32 @@ use ieee.numeric_std.all;
 
 -- Begin with displaying half a blue screen in a process
 architecture behavior of gpu_driver is
-
+    signal x_pos : integer range -145 to 878;
+    signal y_pos : integer range -32 to 991;
+    signal r     : integer range 0 to 15;
+    signal g     : integer range 0 to 15;
+    signal b     : integer range 0 to 15;
 begin
-    process (h_pos, v_pos)
+    x_pos <= to_integer(unsigned(h_pos)) - 145;
+    y_pos <= to_integer(unsigned(v_pos)) - 32;
+    red   <= std_logic_vector(to_unsigned(r, 4));
+    green <= std_logic_vector(to_unsigned(g, 4));
+    blue  <= std_logic_vector(to_unsigned(b, 4));
+
+    process (x_pos, y_pos)
     begin
-        if (to_integer(unsigned(h_pos)) < 145 or to_integer(unsigned(h_pos)) > 784 or to_integer(unsigned(v_pos)) < 32 or to_integer(unsigned(v_pos)) > 511) then
-            red   <= 0;
-            green <= 0;
-            blue  <= 0;
+        if (x_pos < 0 or x_pos > 640 or y_pos < 0 or y_pos > 480) then
+            r <= 0;
+            g <= 0;
+            b <= 0;
+        elsif (y_pos < 470 and y_pos > 383 and x_pos > 10 and x_pos < 66) then
+            r <= 15;
+            g <= 15;
+            b <= 15;
         else
-            red   <= 2;
-            green <= 15;
-            blue  <= 3;
+            r <= 2;
+            g <= 15;
+            b <= 3;
         end if;
     end process;
 end architecture;
--- graphics_driver/gpu_driver_entity.vhd
+++ graphics_driver/gpu_driver_entity.vhd
@@ -3,8 +3,8 @@ entity gpu_driver is
     port (
         h_pos : in std_logic_vector(9 downto 0);
         v_pos : in std_logic_vector(8 downto 0);
-        red   : out integer range 0 to 15;
-        green : out integer range 0 to 15;
-        blue  : out integer range 0 to 15
+        red   : out std_logic_vector(3 downto 0);
+        green : out std_logic_vector(3 downto 0);
+        blue  : out std_logic_vector(3 downto 0);
     );
 end entity gpu_driver;
