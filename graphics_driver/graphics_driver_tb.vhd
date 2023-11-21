bd62df9ac330a069e94e102d11b05369dc6cdd86
Slurpmeester <100770592+Slurpmeester@users.noreply.github.com>
2023-11-21 10:03:10 +0100

    Update graphics_driver_tb.vhd

--- graphics_driver/graphics_driver_tb.vhd
+++ graphics_driver/graphics_driver_tb.vhd
@@ -4,27 +4,32 @@ use ieee.numeric_std.all;
 architecture behaviour of gpu_driver_tb is
     component graphics_driver
         port (
-            h_pos : in std_logic_vector(9 downto 0);
-            v_pos : in std_logic_vector(8 downto 0);
+            h_pos : in std_logic_vector(8 downto 0);
+            v_pos : in std_logic_vector(9 downto 0);
             red   : out integer range 0 to 15;
             green : out integer range 0 to 15;
             blue  : out integer range 0 to 15
         );
     end component;
 
-    signal h_pos : std_logic_vector(9 downto 0);
-    signal v_pos : std_logic_vector(8 downto 0);
+    signal h_pos : std_logic_vector(8 downto 0);
+    signal v_pos : std_logic_vector(9 downto 0);
     signal red   : integer range 0 to 15;
     signal green : integer range 0 to 15;
     signal blue  : integer range 0 to 15;
+
 begin
     test : graphics_driver port map(h_pos, v_pos, red, green, blue);
-    loop_v_pos : for i in 0 to 479 generate
-        loop_h_pos : for j in 0 to 639 generate
-            h_pos <= std_logic_vector(to_unsigned(j, 10));
-            v_pos <= std_logic_vector(to_unsigned(i, 9));
-            wait for 40 ns;
-        end generate;
-    end generate;
-
+	simulation : process
+begin
+     for i in 0 to 479 loop
+        for j in 0 to 639 loop
+		
+		v_pos <= std_logic_vector(to_unsigned(j, 10));
+wait for 40 ns;
+	end loop;
+     	h_pos <= std_logic_vector(to_unsigned(i, 9));
+	
+     end loop;
+end process;
 end architecture;
