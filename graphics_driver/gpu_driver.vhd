1ec112adc80ca5a91a564b1a29b93c09a3fa118a
Ard Geuze <geuzeard@gmail.com>
2023-11-21 10:47:23 +0100

    Reorder v_pos, h_pos

--- graphics_driver/gpu_driver.vhd
+++ graphics_driver/gpu_driver.vhd
@@ -8,7 +8,7 @@ architecture behavior of gpu_driver is
 begin
     process (h_pos, v_pos)
     begin
-        if (to_integer(unsigned(v_pos)) < 145 or to_integer(unsigned(v_pos)) > 784 or to_integer(unsigned(h_pos)) < 32 or to_integer(unsigned(h_pos)) > 511) then
+        if (to_integer(unsigned(h_pos)) < 145 or to_integer(unsigned(h_pos)) > 784 or to_integer(unsigned(v_pos)) < 32 or to_integer(unsigned(v_pos)) > 511) then
             red   <= 0;
             green <= 0;
             blue  <= 0;
