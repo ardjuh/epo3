a0f9b443a21c81b0239bdff6f0f8b82c3eeea0fb
LisanneLaldjising <104197817+LisanneLaldjising@users.noreply.github.com>
2023-11-28 10:04:42 +0100

    Update vga_driver3.vhd

--- vga_driver/vga_driver3.vhd
+++ vga_driver/vga_driver3.vhd
@@ -2,3 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
+
+entity vga_driver is
+    port(clk   	: in std_logic;
+        reset 	: in std_logic;
+        h_pos   : in std_logic;
+        v_pos   : in std_logic;
+        H_sync	: out std_logic;
+        V_sync	: out std_logic;
+        
+	end vga_driver;
