005bc2eaa8343e5b7cce20f3a528a2f04edf364e
Luke <94458418+Sesto-Elemento@users.noreply.github.com>
2023-12-01 15:58:20 +0100

    Update1 controller.vhd

--- controller/controller.vhd
+++ controller/controller.vhd
@@ -5,14 +5,14 @@ use IEEE.numeric_std.all;
 entity mini_controller is
 	port(	clk:			in std_logic;
 			button_select:	in std_logic;
-			button_up:		in std_logic;
-			button_down:	in std_logic;
+--			button_up:	in std_logic;
+--			button_down:	in std_logic;
 			button_left:	in std_logic;
 			button_right:	in std_logic;
 			
 			switch_select:	out std_logic;
-			switch_up: 		out std_logic;
-			switch_down:	out std_logic;
+--			switch_up: 	out std_logic;
+--			switch_down:	out std_logic;
 			switch_left:	out std_logic;
 			switch_right:	out std_logic;
 		);
@@ -23,9 +23,10 @@ architecture behaviour of mini_controller is
 type mini_controller_state is (
 	sela,
 	selb,
-	upa,
-	upb,
-	downa,
+--	upa,
+--	upb,
+--	downa,
+--	downb,
 	lefta,
 	leftb,
 	righta,
@@ -46,31 +47,31 @@ begin
 	end process;
 process;
 
-begin
-	process (clk)
-	begin
-		if (rising_edge (clk)) then
-			if (button_up = '1') then
-				state <= upa;
-			else
-				state <= player_action;
-			end if;
-		end if;
-	end process;
-process;
+--begin
+--	process (clk)
+--	begin
+--		if (rising_edge (clk)) then
+--			if (button_up = '1') then
+--				state <= upa;
+--			else
+--				state <= player_action;
+--			end if;
+--		end if;
+--	end process;
+--process;
 
-begin
-	process (clk)
-	begin
-		if (rising_edge (clk)) then
-			if (button_down = '1') then
-				state <= downa;
-			else
-				state <= player_action;
-			end if;
-		end if;
-	end process;
-process;
+--begin
+--	process (clk)
+--	begin
+--		if (rising_edge (clk)) then
+--			if (button_down = '1') then
+--				state <= downa;
+--			else
+--				state <= player_action;
+--			end if;
+--		end if;
+--	end process;
+--process;
 
 begin
 	process (clk)
@@ -113,13 +114,13 @@ begin
 				if     (button_select = '0') then new_state <= game_resolution;
 					else then new_state <= selb;
 					
-			when downa	=> switch_down <= '1' ;	new_state <= downa;
-				if     (button_down = '1') then new_state <= downb;
-					else new_state <= player_action;
+--			when downa	=> switch_down <= '1' ;	new_state <= downa;
+--				if     (button_down = '1') then new_state <= downb;
+--					else new_state <= player_action;
 					
-			when downb	=> switch_down <= '0' ;	new_state <= downb;
-				if     (button_down = '0') then new_state <= player_action;
-					else then new_state <= downb;
+--			when downb	=> switch_down <= '0' ;	new_state <= downb;
+--				if     (button_down = '0') then new_state <= player_action;
+--					else then new_state <= downb;
 					
 			when lefta	=> switch_left <= '1' ;	new_state <= lefta;
 				if     (button_left = '1') then new_state <= leftb;
@@ -137,13 +138,13 @@ begin
 			if     (button_right = '0') then new_state <= player_action;
 					else then new_state <= rightb;
 					
-			when upa 	=> switch_up <= '1' ;		new_state <= upa;
-				if     (button_up = '1') then new_state <= upb;
-					else new_state <= player_action;
+--			when upa 	=> switch_up <= '1' ;		new_state <= upa;
+--				if     (button_up = '1') then new_state <= upb;
+--					else new_state <= player_action;
 					
-			when upb	=> switch_up <= '0' ;		new_state <= upb;
-				if     (button_up = '0') then new_state <= player_action;
-					else then new_state <= upb;
+--			when upb	=> switch_up <= '0' ;		new_state <= upb;
+--				if     (button_up = '0') then new_state <= player_action;
+--					else then new_state <= upb;
 			end case;	
 		
 	end process;
