3190e89d26b6c56d94d811a6398b4dd07697a512
Ard Geuze <geuzeard@gmail.com>
2023-12-15 16:19:35 +0100

    Add outputs

--- memory/memory_entity.vhd
+++ memory/memory_entity.vhd
@@ -13,5 +13,42 @@ entity memory is
         win_type           : in std_logic_vector(1 downto 0); -- 0: normal, 1: insurance, 2: double down: 3: blackjack
         win_enable         : in std_logic;
         bid                : in std_logic_vector(1 downto 0); -- 0: 2, 1: 6, 2: 10, 3: 20
+        player             : in std_logic_vector(2 downto 0); -- 0: geen player
+
+        player1 : out std_logic;
+        card1_1 : out std_logic_vector (4 downto 0);
+        card1_2 : out std_logic_vector (4 downto 0);
+        card1_3 : out std_logic_vector (4 downto 0);
+        card1_4 : out std_logic_vector (4 downto 0);
+        card1_5 : out std_logic_vector (4 downto 0);
+        money1  : out std_logic_vector (10 downto 0);
+        split1  : out std_logic;
+
+        player2 : out std_logic;
+        card2_1 : out std_logic_vector (4 downto 0);
+        card2_2 : out std_logic_vector (4 downto 0);
+        card2_3 : out std_logic_vector (4 downto 0);
+        card2_4 : out std_logic_vector (4 downto 0);
+        card2_5 : out std_logic_vector (4 downto 0);
+        money2  : out std_logic_vector (10 downto 0);
+        split2  : out std_logic;
+
+        player3 : out std_logic;
+        card3_1 : out std_logic_vector (4 downto 0);
+        card3_2 : out std_logic_vector (4 downto 0);
+        card3_3 : out std_logic_vector (4 downto 0);
+        card3_4 : out std_logic_vector (4 downto 0);
+        card3_5 : out std_logic_vector (4 downto 0);
+        money3  : out std_logic_vector (10 downto 0);
+        split3  : out std_logic;
+
+        player4 : out std_logic;
+        card4_1 : out std_logic_vector (4 downto 0);
+        card4_2 : out std_logic_vector (4 downto 0);
+        card4_3 : out std_logic_vector (4 downto 0);
+        card4_4 : out std_logic_vector (4 downto 0);
+        card4_5 : out std_logic_vector (4 downto 0);
+        money4  : out std_logic_vector (10 downto 0);
+        split4  : out std_logic
     )
 end entity memory;
