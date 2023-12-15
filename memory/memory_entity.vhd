a8c77e7c86bb3a7e19ef6ad2cbf68fa3f6d36301
Ard Geuze <geuzeard@gmail.com>
2023-12-15 16:15:22 +0100

    Add a bunch of inputs

--- memory/memory_entity.vhd
+++ memory/memory_entity.vhd
@@ -1,25 +1,17 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
---in:
-
--- 1 kaart
--- insurance
--- enable-kaart
--- reset
--- double down
--- double down - win
--- Blackjack - win
--- normale win
--- insurance - win
--- bid (2 bits)
-
 entity memory is
     port (
-        rst              : in std_logic;
-        card_enable      : in std_logic;
-        card             : in std_logic_vector(3 downto 0);
-        insurance        : in std_logic;
-        insurance_enable : in std_logic_vector;
+        rst                : in std_logic;
+        card_enable        : in std_logic;
+        card               : in std_logic_vector(3 downto 0);
+        insurance          : in std_logic;
+        insurance_enable   : in std_logic_vector;
+        double_down        : in std_logic;
+        double_down_enable : in std_logic;
+        win_type           : in std_logic_vector(1 downto 0); -- 0: normal, 1: insurance, 2: double down: 3: blackjack
+        win_enable         : in std_logic;
+        bid                : in std_logic_vector(1 downto 0); -- 0: 2, 1: 6, 2: 10, 3: 20
     )
 end entity memory;
