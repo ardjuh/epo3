2815557e846587bf559e0a74a3001954c3222055
Rutger Gosselink <rutger.gosselink@gmail.com>
2023-12-19 11:59:15 +0100

    yoooooo ik ben geen sukkel

--- memory/Player_entity.vhd
+++ memory/Player_entity.vhd
@@ -2,29 +2,42 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity player is
-    port (
-        clk    : in std_logic;
-        rst    : in std_logic;
-        memrst : in std_logic;
+architecture behaviour of player is
+    signal budget_sig : std_logic_vector(9 downto 0);
+begin
+    budget_sig <= budget;
 
-        profit_enable : in std_logic;
-        stake_enable  : in std_logic;
-        profit        : in std_logic_vector(6 downto 0);
-        stake         : in std_logic_vector(4 downto 0);
+    process (clk)
+    begin
 
-        bid_in        : in std_logic_vector(1 downto 0);
-        insurance_in  : in std_logic;
-        doubledown_in : in std_logic;
+        if (rising_edge(clk)) then
+            if (rst = '1') then
+                bid_out        <= "00";
+                budget         <= "0000000000";
+                insurance_out  <= '0';
+                doubledown_out <= '0';
+            elsif (memrst = '1') then
+                bid_out        <= "00";
+                insurance_out  <= '0';
+                doubledown_out <= '0';
+            else
 
-        bid_in_enable        : in std_logic;
-        insurance_in_enable  : in std_logic;
-        doubledown_in_enable : in std_logic;
-
-        bid_out        : out std_logic_vector(1 downto 0);
-        budget         : out std_logic_vector(9 downto 0);
-        insurance_out  : out std_logic;
-        doubledown_out : out std_logic
-    );
-end
-entity;
+                if (profit_enable = '1') then
+                    budget <= std_logic_vector(unsigned(budget_sig) + unsigned(profit));
+                end if;
+                if (stake_enable = '1') then
+                    budget <= std_logic_vector(unsigned(budget_sig) - unsigned(stake));
+                end if;
+                if (bid_in_enable = '1') then
+                    bid_out <= bid_in;
+                end if;
+                if (insurance_in_enable = '1') then
+                    insurance_out <= insurance_in;
+                end if;
+                if (doubledown_in_enable = '1') then
+                    doubledown_out <= doubledown_in;
+                end if;
+            end if;
+        end if;
+    end process;
+end behaviour;
