c62e7d13af73ce85e14fdd0f93a9a74d13b8e533
Wilson <98850345+wilsonrng@users.noreply.github.com>
2023-12-22 13:16:31 +0100

    Update deck_mem_tb.vhdl WORKING

--- rng_counter/deck-mem/deck_mem_tb.vhdl
+++ rng_counter/deck-mem/deck_mem_tb.vhdl
@@ -3,30 +3,34 @@ use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of deck_mem_tb is
    component deck_mem
-      port(reset : in std_logic;
-   	random_num  : in  std_logic_vector(7 downto 0);
+      port(clk		: in std_logic;
+   	reset       : in  std_logic;
+           random_num  : in  std_logic_vector(7 downto 0);
            shuffle     : in  std_logic;
            random_card : out std_logic_vector(3 downto 0));
    end component;
-   signal reset : std_logic;
+   signal clk		: std_logic;
+   signal reset       : std_logic;
    signal random_num  : std_logic_vector(7 downto 0);
    signal shuffle     : std_logic;
    signal random_card : std_logic_vector(3 downto 0);
 begin
-   test: deck_mem port map (reset, random_num, shuffle, random_card);
+   test: deck_mem port map (clk, reset, random_num, shuffle, random_card);
+   clk <= '0' after 0 ns,
+          '1' after 20 ns when clk /= '1' else '0' after 20 ns;
    reset <= '1' after 0 ns,
-            '0' after 10 ns;
-   random_num <= "00000000" after 0 ns,
+            '0' after 40 ns;
+   random_num <=	"00000000" after 0 ns,
 		"00101111" after 100 ns, --47
-		"00000000" after 110 ns,
-		"10111101" after 200 ns; --189
-		--"00000000" after 300 ns,
-		--"00000000" after 400 ns,
-		--"00000000" after 500 ns,
-		--"00000000" after 600 ns,
-		--"00000000" after 700 ns,
-		--"00000000" after 800 ns,
-		--"00000000" after 900 ns,
-		--"00000000" after 1000 ns,
-   shuffle <= '0' after 0 ns;
+		"00000000" after 140 ns,
+		"00101111" after 190 ns, --47
+		"00000000" after 230 ns,
+		"00101111" after 280 ns, --47
+		"00000000" after 320 ns,
+		"10111101" after 360 ns,	--189
+		"00000000" after 400 ns;
+
+   shuffle <=		'0' after 0 ns,
+		'1' after 500 ns,
+		'0' after 550 ns;
 end behaviour;
