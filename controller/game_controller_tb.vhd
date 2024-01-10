464ef0a584c76a712e6d44fe2dc6c3299a2dc439
amvermeer <152719164+amvermeer@users.noreply.github.com>
2024-01-10 15:52:32 +0100

    Update game_controller_tb.vhd

--- controller/game_controller_tb.vhd
+++ controller/game_controller_tb.vhd
@@ -6,14 +6,14 @@ end entity game_controller_tb;
 
 architecture behaviour of game_controller_tb is
 
-entity mini_con is
-   port(clk    			: in  std_logic;
-        reset  			: in  std_logic;
-        button_left			: in  std_logic;
-	button_right		: in  std_logic;
-	button_select		: in  std_logic;
-
-        switch_left			: out std_logic;
-	switch_right		: out std_logic;
-	switch_select		: out std_logic);
-end mini_con;
+component game_controller_tb                          --------- portmap placeholder until the compiled code is uploaded ---------
+      port(	clk    		: in  std_logic;
+           	reset  		: in  std_logic;
+           	button_left	: in  std_logic;
+   		button_right	: in  std_logic;
+   		button_select	: in  std_logic;
+   
+           	switch_left	: out std_logic;
+   		switch_right	: out std_logic;
+   		switch_select	: out std_logic);
+   end component;
