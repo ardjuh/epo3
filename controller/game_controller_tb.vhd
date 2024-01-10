505cdff3f8cf2955ae4de35227276f277ffc038d
amvermeer <152719164+amvermeer@users.noreply.github.com>
2024-01-10 15:50:32 +0100

    Update game_controller_tb.vhd

--- controller/game_controller_tb.vhd
+++ controller/game_controller_tb.vhd
@@ -5,3 +5,15 @@ entity game_controller_tb is
 end entity game_controller_tb;
 
 architecture behaviour of game_controller_tb is
+
+entity mini_con is
+   port(clk    			: in  std_logic;
+        reset  			: in  std_logic;
+        button_left			: in  std_logic;
+	button_right		: in  std_logic;
+	button_select		: in  std_logic;
+
+        switch_left			: out std_logic;
+	switch_right		: out std_logic;
+	switch_select		: out std_logic);
+end mini_con;
