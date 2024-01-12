9e2d36dd98be6eb3957a000793cab7811b1dfb6c
Ard Geuze <geuzeard@gmail.com>
2024-01-12 14:54:46 +0100

    Format

--- memory/memory_entity_tb.vhd
+++ memory/memory_entity_tb.vhd
@@ -6,7 +6,7 @@ architecture memory_tb_arc of memory_tb is
         port (
             clk               : in std_logic;
             rst               : in std_logic;
-	    end_round		      : in std_logic;
+            end_round         : in std_logic;
             card_enable       : in std_logic;
             card              : in std_logic_vector(3 downto 0);
             insurance         : in std_logic;
@@ -17,14 +17,14 @@ architecture memory_tb_arc of memory_tb is
             win_enable        : in std_logic;
             bid               : in std_logic_vector(1 downto 0); -- 0: 2, 1: 6, 2: 10, 3: 20
             bid_enable        : in std_logic;
-            player_in            : in std_logic_vector(2 downto 0); -- 0: geen player, 1-4: speler, 5: dealer, 6: split 
+            player_in         : in std_logic_vector(2 downto 0); -- 0: geen player, 1-4: speler, 5: dealer, 6: split 
             player_enable     : in std_logic;
             money             : in std_logic_vector(9 downto 0);
             split             : in std_logic;
 
             player_out : out std_logic_vector(2 downto 0);
 
-            player_a     : out std_logic;
+            player_a    : out std_logic;
             card1_1     : out std_logic_vector(3 downto 0);
             card1_2     : out std_logic_vector(3 downto 0);
             card1_3     : out std_logic_vector(3 downto 0);
@@ -35,9 +35,9 @@ architecture memory_tb_arc of memory_tb is
             split1      : out std_logic;
             insurance1  : out std_logic;
             doubledown1 : out std_logic;
-            score1   : out std_logic_vector(4 downto 0);
+            score1      : out std_logic_vector(4 downto 0);
 
-            player_b     : out std_logic;
+            player_b    : out std_logic;
             card2_1     : out std_logic_vector(3 downto 0);
             card2_2     : out std_logic_vector(3 downto 0);
             card2_3     : out std_logic_vector(3 downto 0);
@@ -48,9 +48,9 @@ architecture memory_tb_arc of memory_tb is
             split2      : out std_logic;
             insurance2  : out std_logic;
             doubledown2 : out std_logic;
-            score2   : out std_logic_vector(4 downto 0);
+            score2      : out std_logic_vector(4 downto 0);
 
-            player_c     : out std_logic;
+            player_c    : out std_logic;
             card3_1     : out std_logic_vector(3 downto 0);
             card3_2     : out std_logic_vector(3 downto 0);
             card3_3     : out std_logic_vector(3 downto 0);
@@ -61,9 +61,9 @@ architecture memory_tb_arc of memory_tb is
             split3      : out std_logic;
             insurance3  : out std_logic;
             doubledown3 : out std_logic;
-            score3   : out std_logic_vector(4 downto 0);
+            score3      : out std_logic_vector(4 downto 0);
 
-            player_d     : out std_logic;
+            player_d    : out std_logic;
             card4_1     : out std_logic_vector(3 downto 0);
             card4_2     : out std_logic_vector(3 downto 0);
             card4_3     : out std_logic_vector(3 downto 0);
@@ -74,7 +74,7 @@ architecture memory_tb_arc of memory_tb is
             split4      : out std_logic;
             insurance4  : out std_logic;
             doubledown4 : out std_logic;
-            score4   : out std_logic_vector(4 downto 0);
+            score4      : out std_logic_vector(4 downto 0);
 
             -- dealer
             card5_1 : out std_logic_vector(3 downto 0);
@@ -82,7 +82,7 @@ architecture memory_tb_arc of memory_tb is
             card5_3 : out std_logic_vector(3 downto 0);
             card5_4 : out std_logic_vector(3 downto 0);
             card5_5 : out std_logic_vector(3 downto 0);
-            score5   : out std_logic_vector(4 downto 0);
+            score5  : out std_logic_vector(4 downto 0);
 
             -- split
             card6_1 : out std_logic_vector(3 downto 0);
@@ -90,24 +90,24 @@ architecture memory_tb_arc of memory_tb is
             card6_3 : out std_logic_vector(3 downto 0);
             card6_4 : out std_logic_vector(3 downto 0);
             card6_5 : out std_logic_vector(3 downto 0);
-            score6   : out std_logic_vector(4 downto 0)
+            score6  : out std_logic_vector(4 downto 0)
         );
     end component;
-    signal rst, clk, end_round          : std_logic := '0';
-    signal card_enable       : std_logic := '0';
-    signal card              : std_logic_vector(3 downto 0) := "0000";
-    signal insurance         : std_logic := '0';
-    signal insurance_enable  : std_logic := '0';
-    signal doubledown        : std_logic := '0';
-    signal doubledown_enable : std_logic := '0';
-    signal win_type          : std_logic_vector(1 downto 0) := "00"; -- 0: normal, 1: insurance, 2: double down: 3: blackjack
-    signal win_enable        : std_logic := '0';
-    signal bid               : std_logic_vector(1 downto 0) := "00"; -- 0: 2, 1: 6, 2: 10, 3: 20
-    signal bid_enable        : std_logic := '0';
-    signal player_in            : std_logic_vector(2 downto 0) := "000"; -- 0: geen player, 1-4: speler, 5: dealer, 6: split 
-    signal player_enable     : std_logic := '0';
-    signal money             : std_logic_vector(9 downto 0) := "0000000000";
-    signal split             : std_logic := '0';
+    signal rst, clk, end_round : std_logic                    := '0';
+    signal card_enable         : std_logic                    := '0';
+    signal card                : std_logic_vector(3 downto 0) := "0000";
+    signal insurance           : std_logic                    := '0';
+    signal insurance_enable    : std_logic                    := '0';
+    signal doubledown          : std_logic                    := '0';
+    signal doubledown_enable   : std_logic                    := '0';
+    signal win_type            : std_logic_vector(1 downto 0) := "00"; -- 0: normal, 1: insurance, 2: double down: 3: blackjack
+    signal win_enable          : std_logic                    := '0';
+    signal bid                 : std_logic_vector(1 downto 0) := "00"; -- 0: 2, 1: 6, 2: 10, 3: 20
+    signal bid_enable          : std_logic                    := '0';
+    signal player_in           : std_logic_vector(2 downto 0) := "000"; -- 0: geen player, 1-4: speler, 5: dealer, 6: split 
+    signal player_enable       : std_logic                    := '0';
+    signal money               : std_logic_vector(9 downto 0) := "0000000000";
+    signal split               : std_logic                    := '0';
 
     signal player_out : std_logic_vector(2 downto 0);
 
@@ -122,7 +122,7 @@ architecture memory_tb_arc of memory_tb is
     signal split1      : std_logic;
     signal insurance1  : std_logic;
     signal doubledown1 : std_logic;
-    signal score1   : std_logic_vector(4 downto 0);
+    signal score1      : std_logic_vector(4 downto 0);
 
     signal player2     : std_logic;
     signal card2_1     : std_logic_vector(3 downto 0);
@@ -135,7 +135,7 @@ architecture memory_tb_arc of memory_tb is
     signal split2      : std_logic;
     signal insurance2  : std_logic;
     signal doubledown2 : std_logic;
-    signal score2   : std_logic_vector(4 downto 0);
+    signal score2      : std_logic_vector(4 downto 0);
 
     signal player3     : std_logic;
     signal card3_1     : std_logic_vector(3 downto 0);
@@ -148,7 +148,7 @@ architecture memory_tb_arc of memory_tb is
     signal split3      : std_logic;
     signal insurance3  : std_logic;
     signal doubledown3 : std_logic;
-    signal score3   : std_logic_vector(4 downto 0);
+    signal score3      : std_logic_vector(4 downto 0);
 
     signal player4     : std_logic;
     signal card4_1     : std_logic_vector(3 downto 0);
@@ -161,7 +161,7 @@ architecture memory_tb_arc of memory_tb is
     signal split4      : std_logic;
     signal insurance4  : std_logic;
     signal doubledown4 : std_logic;
-    signal score4   : std_logic_vector(4 downto 0);
+    signal score4      : std_logic_vector(4 downto 0);
 
     -- dealer
     signal card5_1 : std_logic_vector(3 downto 0);
@@ -181,9 +181,9 @@ architecture memory_tb_arc of memory_tb is
 begin
 
     test : memory port map(
-clk,
+        clk,
         rst,
-	end_round,
+        end_round,
         card_enable,
         card,
         insurance,
@@ -212,7 +212,7 @@ clk,
         split1,
         insurance1,
         doubledown1,
-	score1,
+        score1,
 
         player2,
         card2_1,
@@ -225,7 +225,7 @@ clk,
         split2,
         insurance2,
         doubledown2,
-	score2,
+        score2,
 
         player3,
         card3_1,
@@ -238,7 +238,7 @@ clk,
         split3,
         insurance3,
         doubledown3,
-	score3,
+        score3,
 
         player4,
         card4_1,
@@ -251,7 +251,7 @@ clk,
         split4,
         insurance4,
         doubledown4,
-	score4,
+        score4,
 
         -- dealer
         card5_1,
@@ -259,7 +259,7 @@ clk,
         card5_3,
         card5_4,
         card5_5,
-	score5,
+        score5,
 
         -- split
         card6_1,
@@ -267,10 +267,8 @@ clk,
         card6_3,
         card6_4,
         card6_5,
-	score6
-);
-
-   
+        score6
+    );
     clk <= not clk after 10 ns;
 
     card <= "0000" after 0 ns,
@@ -286,18 +284,18 @@ clk,
         "0101" after 210 ns;
 
     card_enable <= '0' after 0 ns,
-    '1' after 30 ns,
-    '0' after 90 ns,
-    '1' after 110 ns,
-    '0' after 190 ns,
-    '1' after 210 ns,
-    '0' after 230 ns;
+        '1' after 30 ns,
+        '0' after 90 ns,
+        '1' after 110 ns,
+        '0' after 190 ns,
+        '1' after 210 ns,
+        '0' after 230 ns;
 
     player_in <= "000" after 0 ns,
-    "010" after 90 ns,
-    "001" after 110 ns,
-    "000" after 190 ns,
-    "010" after 210 ns;
+        "010" after 90 ns,
+        "001" after 110 ns,
+        "000" after 190 ns,
+        "010" after 210 ns;
 
     rst <= '0' after 0 ns,
         '1' after 5 ns,
