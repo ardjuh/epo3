ece02f8379ce5ec529e093564b0d6e94f6d9bea5
Fkrete <131753232+fkrete@users.noreply.github.com>
2024-01-16 11:23:50 +0100

    Update gpu_driver_entity.vhd

--- graphics_driver/gpu_driver_entity.vhd
+++ graphics_driver/gpu_driver_entity.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-
 -- rgb output, 4 bits per color, hsync, vsync
 entity gpu_driver is
     port (
@@ -9,9 +8,14 @@ entity gpu_driver is
         red   : out std_logic_vector(3 downto 0);
         green : out std_logic_vector(3 downto 0);
         blue  : out std_logic_vector(3 downto 0);
-	screentype : in std_logic_vector(1 downto 0);
 
-        player : in std_logic_vector (1 downto 0);
+        player : in std_logic_vector (2 downto 0);
+
+        em : in std_logic;
+        double : in std_logic;
+        insurance : in std_logic;
+        split : in std_logic;
+        hit   : in std_logic;
 
         player_a    : in std_logic;
         card1_1     : in std_logic_vector (3 downto 0);
@@ -24,7 +28,6 @@ entity gpu_driver is
         bet1        : in std_logic_vector (1 downto 0);
         insurance1 : in std_logic;
         doubledown1 : in std_logic;
-
         player_b    : in std_logic;
         card2_1     : in std_logic_vector (3 downto 0);
         card2_2     : in std_logic_vector (3 downto 0);
@@ -36,7 +39,6 @@ entity gpu_driver is
         bet2        : in std_logic_vector (1 downto 0);
         insurance2 : in std_logic;
         doubledown2 : in std_logic;
-
         player_c    : in std_logic;
         card3_1     : in std_logic_vector (3 downto 0);
         card3_2     : in std_logic_vector (3 downto 0);
@@ -48,7 +50,6 @@ entity gpu_driver is
         bet3        : in std_logic_vector (1 downto 0);
         insurance3 : in std_logic;
         doubledown3 : in std_logic;
-
         player_d    : in std_logic;
         card4_1     : in std_logic_vector (3 downto 0);
         card4_2     : in std_logic_vector (3 downto 0);
@@ -56,9 +57,9 @@ entity gpu_driver is
         card4_4     : in std_logic_vector (3 downto 0);
         card4_5     : in std_logic_vector (3 downto 0);
         money4      : in std_logic_vector (10 downto 0);
-        split4      : in std_logic;
-        bet4        : in std_logic_vector (1 downto 0);
-        insurance4 : in std_logic;
+        split4      : in std_logic
+        bet4        : in std_logic_vector (1 downto 0)
+        insurance4 : in std_logic
         doubledown4 : in std_logic
     );
 end entity gpu_driver;
