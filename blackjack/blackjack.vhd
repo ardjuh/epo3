78cfe1e468d861cdbcbb26dd26cfc1db0ca8444d
LisanneLaldjising <104197817+LisanneLaldjising@users.noreply.github.com>
2024-01-16 12:10:57 +0100

    Update blackjack.vhd

--- blackjack/blackjack.vhd
+++ blackjack/blackjack.vhd
@@ -272,12 +272,13 @@ end component;
 	signal bid1_signal, bid1_signal_new, bid2_signal, bid2_signal_new, bid3_signal, bid3_signal_new, bid4_signal, bid4_signal_new : std_logic_vector(1 downto 0); --bids per player
 	signal card1_1_signal, card1_2_signal, card1_3signal, card1_4_sigal, card1_5_signal, card2_1_signal, card2_2_signal, card2_3_signal, card2_4_signal, card2_5_signal, card3_1_signal, card3_2_signal, card3_3_signal, card3_4_signal, card3_5_signal, card4_1_signal, card4_2_signal, card4_3_signal, card4_4_signal, card4_5_signal, card5_1_signal, card5_2_signal, card5_3_signal, card5_4_signal, card5_5_signal, card6_1_signal, card6_2_signal, card6_3_signal, card6_4_signal, card6_5_signal : std_logic_vector(3 downto 0); -- hand cards of players including dealers hand
 	signal money1_signal, money1_signal_new, money2_signal, money2_signal_new money3_signal, money3_signal_new, moneyd4_signal, money4_signal_new : std_logic_vector(10 downto 0); -- money of all 4 players
-	--signal split1_signal, split2_signal, split3_signal, split4_signal : std_logic;	 -- high if a player splits, low if a player does not split
+	signal split1_signal, split2_signal, split3_signal, split4_signal : std_logic;	 -- high if a player splits, low if a player does not split
 	signal insurance1_signal, insurance2_signal, insurance3_signal, insurance4_signal- : std_logic; -- if a players chooses insurance
 	signal double1, double2, double3, double4 : std_logic;
 	signal player_current_signal, player_signal_new : std_logic_vector(2 downto 0); -- number of players
 	signal player_turn_in_signal, player_turn_new_signal : std_logic_vector(2 downto 0); -- which player is at turn
-	signal player_a_signal, player_b_signal, player_c_signal, player_d_signal : std_logic; -- the different players
+	signal player_a_in_signal, player_b_in_signal, player_c_in_signal, player_d_in_signal : std_logic;-- the different players
+	signal player_a_out_signal, player_b_out_signal, player_c_out_signal, player_d_out_signal : std_logic;
 	signal select_signal, left_signal, right_signal,  H_sync_signal, V_sync_signal : std_logic;  -- gamepad inputs controller and sync signals vga driver
 	signal x_pos_signal, y_pos_signal : std_logic_vector(9 downto 0); -- current horizontal and vertical pixel position
 	signal red_signal, green_signal, blue_signal : std_logic_vector(3 downto 0); -- rgb signals
@@ -289,28 +290,35 @@ end component;
 	signal draw_screen_signal : std_logic_vector(1 downto 0);
 	signal cursor_position_signal: std_logic_vector(2 downto 0);
 	signal win_type_signal : std_logic_vector(2 downto 0);  -- 0: niet gewonnen, 1: insurance, 2: double down, 3: blackjack, 4 : normal
+	signal player1_score, player2_score, player3_score, player4_score, dealer_score, split_score : std_logic_vector (4 downto 0);
 begin
 	p1: memory 	port map(clk => clk, rst => reset, 
-				enable => enable_signal, 
-				insurance => insurance_signal,
-				doubledown => double_signal,
-				win_type => win_type_signal -- mem ontvangt van controller
+				enable => enable_signal, -- from controller
 				player_in => player_signal_new, -- comes from controller, number of players
-				player_out => player_current_signal, -- goes to controller, number of players
+				insurance => insurance_signal, -- from controller
+				doubledown => double_signal, -- from controller
+				win_type => win_type_signal, -- mem ontvangt van controller
+				split => split_signal, -- from controller
+				player_a_in => player_a_in_signal, player_b_in=> player_b_in_signal, player_c_in => player_c_in_signal, player_d_in => player_d_in_signal, -- van waar komt dit?
+				money1_in => money1_singal_new, money2_in => money2_signal_new, money3_in => money3_signal_new, money4_in => money4_signal_new,  -- from controller to mem
+				bid1_in => bid1_signal_new, bid2_in => bid2_signal_new, bid3_in => bid3_signal_new, bid4_in => bid4_signal_new, -- from controller to mem
+
+				player_out => player_current_signal, -- goes to controller, number of players 
 				 
-				player_a_in => player_a_signal, player_b_in=> player_b_signal, player_c_in => player_c_signal, player_d_in => player_d_signal,
-				 
-				bid1_in => bid1_signal, bid2_in => bid2_signal, bid3_in => bid3_signal, bid4_in => bid4_signal, -- comes from controller
-				split => split_signal -- comes from controller if someone decides to split
-				insurance1 => insurance1_signal, insurance2 => insurance2_signal, insurance3 => insurance3_signal, insurance4 => insurance4_signal,
-				doubledown1 => double1, doubledown2 => double2, doubledown3 => double3, doubledown4 => double4,
-				money1_in => money1_singal, money2_in => money2_signal, money3_in => money3_signal, money4_in => money4_signal,
-			    	card1_1 => card1_1_signal, card1_2 => card1_2_signal, card1_3 => card1_3_signal, card1_4 => card1_4_signal, card1_5 => card1_5_signal,
-			    	card2_1 => card2_1_signal, card2_2 => card2_2_signal, card2_3 => card2_3_signal, card2_4 => card2_4_signal, card2_5 => card2_5_signal,
-			    	card3_1 => card3_1_signal, card3_2 => card3_2_signal, card3_3 => card3_3_signal, card3_4 => card3_4_signal, card3_5 => card3_5_signal,
-			    	card4_1 => card4_1_signal, card4_2 => card4_2_signal, card4_3 => card4_3_signal, card4_4 => card4_4_signal, card4_5 => card4_5_signal,
-			    	card5_1 => card5_1_signal, card5_2 => card5_2_signal, card5_3 => card5_3_signal, card5_4 => card5_4_signal, card5_5 => card5_5_signal,
-				card6_1 => card6_1_signal, card6_2 => card6_2_signal, card6_3 => card6_3_signal, card6_4 => card6_4_signal, card6_5 => card6_5_signal,
+				player_a_out => player_a_out_signal, player_b_out=> player_b_out_signal, player_c_out => player_c_out_signal, player_d_out => player_d_out_signal, -- to gpu
+				money1_out => money1_signal, money2_out => money2_signal, money3_out => money3_signal, money4_out => money4_signal, -- goes to controller and gpu
+				bid1_out => bid1_signal, bid2_out => bid2_signal, bid3_out => bid3_signal, bid4_out => bid4_signal, -- goes to controller and gpu
+				split1_out => split1_signal, split2_out => split2_signal, split3_out => split3_signal, split4_out => split4_signal, -- only goes to gpu
+				insurance1_out => insurance1_signal, insurance2_out => insurance2_signal, insurance3_out => insurance3_signal, insurance4_out => insurance4_signal, -- only goes to gpu
+				doubledown1_out => double1, doubledown2_out => double2 => doubledown3_out => double3, doubledown4_out => double4, -- only goes to gpu
+				score1_out => player1_score, score2_out => player2_score, score3_out => player3_score, score4_out => player4_score, score5_out => dealer_score, score6_out => split_score, -- goes to controller
+
+			    	card1_1_out => card1_1_signal, card1_2_out => card1_2_signal, card1_3_out => card1_3_signal, card1_4_out => card1_4_signal, card1_5_out => card1_5_signal,
+			    	card2_1_out => card2_1_signal, card2_2_out => card2_2_signal, card2_3_out => card2_3_signal, card2_4_out => card2_4_signal, card2_5_out => card2_5_signal,
+			    	card3_1_out => card3_1_signal, card3_2_out => card3_2_signal, card3_3_out => card3_3_signal, card3_4_out => card3_4_signal, card3_5_out => card3_5_signal,
+			    	card4_1_out => card4_1_signal, card4_2_out => card4_2_signal, card4_3_out => card4_3_signal, card4_4_out => card4_4_signal, card4_5_out => card4_5_signal,
+			    	card5_1_out => card5_1_signal, card5_2_out => card5_2_signal, card5_3_out => card5_3_signal, card5_4_out => card5_4_signal, card5_5_out => card5_5_signal,
+				card6_1_out => card6_1_signal, card6_2_out => card6_2_signal, card6_3_out => card6_3_signal, card6_4_out => card6_4_signal, card6_5_out => card6_5_signal,
 				
 
 	p2: controller 	port map(clk => clk, reset => reset, 
