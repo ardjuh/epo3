b6b758aa34f8d14211cda2ccfd3353b9e22d0dcb
Fkrete <131753232+fkrete@users.noreply.github.com>
2024-01-16 16:25:53 +0100

    Update vga_driver_combined.vhd

--- vga_driver/vga_driver_combined.vhd
+++ vga_driver/vga_driver_combined.vhd
@@ -3,16 +3,6 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
-entity vga_driver_combined is
-  port(clk : in std_logic;
-       reset: in std_logic;
-       x_pos: out std_logic_vector (9 downto 0);
-       y_pos: out std_logic_vector (9 downto 0);
-       H_sync: out std_logic;
-       V_sync: out std_logic);
-
-end vga_driver_combined;
-
 architecture behavioral of vga_driver_combined is
 
   component counter 
