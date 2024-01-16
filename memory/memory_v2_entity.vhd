ca517f2c1c8092c19210247eb59e7da2bc73776d
Ard Geuze <geuzeard@gmail.com>
2024-01-16 16:08:29 +0100

    Add bid enable

--- memory/memory_v2_entity.vhd
+++ memory/memory_v2_entity.vhd
@@ -3,10 +3,11 @@ use IEEE.std_logic_1164.all;
 
 entity memory is
     port (
-        clk       : in std_logic;
-        rst       : in std_logic;
-        enable    : in std_logic;
-        player_in : in std_logic_vector(2 downto 0);
+        clk        : in std_logic;
+        rst        : in std_logic;
+        enable     : in std_logic;
+        bid_enable : in std_logic;
+        player_in  : in std_logic_vector(2 downto 0);
 
         insurance  : in std_logic;
         doubledown : in std_logic;
