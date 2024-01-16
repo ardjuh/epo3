3867339c6a30420e26eacade195f9c15235a92b7
Fkrete <131753232+fkrete@users.noreply.github.com>
2024-01-16 16:24:06 +0100

    Update vga_driver_combined.vhd

--- vga_driver/vga_driver_combined.vhd
+++ vga_driver/vga_driver_combined.vhd
@@ -23,19 +23,20 @@ architecture behavioral of vga_driver_combined is
   end component;
 
   component vga_driver
-    port(clk : in std_logic;
+    port(
          reset : in std_logic;
          h_pos : in std_logic_vector(9 downto 0);
          v_pos : in std_logic_vector(9 downto 0);
          H_sync: out std_logic;
          V_sync: out std_logic);
   end component;
+
   signal m1, m2 : std_logic_vector(9 downto 0);
-  signal m3, m4 : std logic;
+  signal m3, m4 : std_logic;
 begin
 
   c1: counter port map(clk => clk, reset => reset, x_pos => m1, y_pos => m2);
-  c2: vga_driver port map( clk => clk, reset => reset, h_pos => m1, v_pos =>m2, H_sync => m3, V_sync => m4);
+  c2: vga_driver port map(reset => reset, h_pos => m1, v_pos =>m2, H_sync => m3, V_sync => m4);
 
   x_pos <= m1;
   y_pos <= m2;
