92f0902926f902f8830486c39f91d97811d38b3e
Dany22222 <151168002+Dany22222@users.noreply.github.com>
2024-01-18 14:20:08 +0100

    Update and rename Handentity_appendix to Handentity_appendix.vhdl

--- memory/Handentity_appendix.vhdl
+++ memory/Handentity_appendix.vhdl
@@ -2,6 +2,27 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+entity hand is
+    port (
+        clk    : in std_logic;
+        rst    : in std_logic;
+        enable : in std_logic;
+        card   : in std_logic_vector(3 downto 0);
+        card1  : out std_logic_vector(3 downto 0);
+        card2  : out std_logic_vector(3 downto 0);
+        card3  : out std_logic_vector(3 downto 0);
+        card4  : out std_logic_vector(3 downto 0);
+        card5  : out std_logic_vector(3 downto 0);
+        score  : out std_logic_vector(4 downto 0) -- "11111" is BlackJack
+    );
+end hand;
+
+
+
+
+
+
+
 architecture behaviour of hand is
 	signal card1_i, card2_i, card3_i, card4_i, card5_i : std_logic_vector(3 downto 0);
 begin
