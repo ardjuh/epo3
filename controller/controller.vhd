4a0665442b8a102d058b21f4e22ec20b9af60054
ardjuh <63558466+ardjuh@users.noreply.github.com>
2024-01-22 11:48:28 +0100

    Update controller.vhd

--- controller/controller.vhd
+++ controller/controller.vhd
@@ -1,5 +1,122 @@
 library IEEE;
-use IEEE.std_logic_1164.ALL;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity controller is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic;
+
+
+        --Player_Turn	: in std_logic_vector (2 downto 0);
+
+        switch_select : in std_logic;
+        switch_left   : in std_logic; -- player inputs --
+        switch_right  : in std_logic;
+
+        Player1_Budget : in std_logic_vector (9 downto 0); -- base budget is 100, score limit chosen as 1000 so 10 bits --
+        Player2_Budget : in std_logic_vector (9 downto 0);
+        Player3_Budget : in std_logic_vector (9 downto 0);
+        Player4_Budget : in std_logic_vector (9 downto 0);
+
+        Player1_Bid : in std_logic_vector (1 downto 0); -- Bid and Budget required to determine if Insurance/Double are possible --
+        Player2_Bid : in std_logic_vector (1 downto 0); -- Value of Initial Bid = 2,6,10,20 -> 00,01,10,11 (Internal signal Bid_Value) --
+        Player3_Bid : in std_logic_vector (1 downto 0); -- Controller never needs the augmented value of Bid as Double/Insurance/Split --
+        Player4_Bid : in std_logic_vector (1 downto 0); -- are Turn 1 actions (If Mem Controller does end-round calculations) --
+
+        Player1_Insured : in std_logic;
+        Player2_Insured : in std_logic;
+        Player3_Insured : in std_logic;
+        Player4_Insured : in std_logic;
+
+        Player1_Doubled_Down : in std_logic;
+        Player2_Doubled_Down : in std_logic;
+        Player3_Doubled_Down : in std_logic;
+        Player4_Doubled_Down : in std_logic;
+
+        Player1_Hand_Card_1 : in std_logic_vector (3 downto 0); -- Each card is a 4-bit vector --
+        Player1_Hand_Card_2 : in std_logic_vector (3 downto 0);
+        Player1_Hand_Card_3 : in std_logic_vector (3 downto 0);
+        Player1_Hand_Card_4 : in std_logic_vector (3 downto 0);
+        Player1_Hand_Card_5 : in std_logic_vector (3 downto 0);
+        Player1_Hand_Score  : in std_logic_vector (4 downto 0); -- Player can have 20 and draw a 10, so 30 points total possible --
+
+        Player2_Hand_Card_1 : in std_logic_vector (3 downto 0);
+        Player2_Hand_Card_2 : in std_logic_vector (3 downto 0);
+        Player2_Hand_Card_3 : in std_logic_vector (3 downto 0);
+        Player2_Hand_Card_4 : in std_logic_vector (3 downto 0);
+        Player2_Hand_Card_5 : in std_logic_vector (3 downto 0);
+        Player2_Hand_Score  : in std_logic_vector (4 downto 0);
+
+        Player3_Hand_Card_1 : in std_logic_vector (3 downto 0);
+        Player3_Hand_Card_2 : in std_logic_vector (3 downto 0);
+        Player3_Hand_Card_3 : in std_logic_vector (3 downto 0);
+        Player3_Hand_Card_4 : in std_logic_vector (3 downto 0);
+        Player3_Hand_Card_5 : in std_logic_vector (3 downto 0);
+        Player3_Hand_Score  : in std_logic_vector (4 downto 0);
+
+        Player4_Hand_Card_1 : in std_logic_vector (3 downto 0);
+        Player4_Hand_Card_2 : in std_logic_vector (3 downto 0);
+        Player4_Hand_Card_3 : in std_logic_vector (3 downto 0);
+        Player4_Hand_Card_4 : in std_logic_vector (3 downto 0);
+        Player4_Hand_Card_5 : in std_logic_vector (3 downto 0);
+        Player4_Hand_Score  : in std_logic_vector (4 downto 0);
+
+        Dealer_Hand_Card_1 : in std_logic_vector (3 downto 0);
+        Dealer_Hand_Card_2 : in std_logic_vector (3 downto 0);
+        Dealer_Hand_Card_3 : in std_logic_vector (3 downto 0);
+        Dealer_Hand_Card_4 : in std_logic_vector (3 downto 0);
+        Dealer_Hand_Card_5 : in std_logic_vector (3 downto 0);
+        Dealer_Hand_Score  : in std_logic_vector (4 downto 0);
+
+        Reserve_Hand_Card_1 : in std_logic_vector (3 downto 0); -- Reserve hand for Split. Only one player can split (low chance of multiple splits) --
+        Reserve_Hand_Card_2 : in std_logic_vector (3 downto 0);
+        Reserve_Hand_Card_3 : in std_logic_vector (3 downto 0);
+        Reserve_Hand_Card_4 : in std_logic_vector (3 downto 0);
+        Reserve_Hand_Card_5 : in std_logic_vector (3 downto 0);
+        Reserve_Hand_Score  : in std_logic_vector (4 downto 0);
+
+        random_card  : in std_logic_vector (3 downto 0); -- Comms with RNG --
+        request_card : out std_logic;
+        new_card     : out std_logic_vector (3 downto 0); -- Mem Controller determines where the new card goes from Receiving Hand and Hand Cards --
+
+        cursor_position     : out std_logic_vector(2 downto 0);
+        current_screen_type : out std_logic_vector(1 downto 0);
+
+        hit_option        : out std_logic;
+        double_option     : out std_logic;
+        split_option      : out std_logic;
+        insurance_option  : out std_logic;
+        even_money_option : out std_logic;
+
+        Player1_Bid_New : out std_logic_vector (1 downto 0); -- 2,6,10,20 = 4 options so 2 bits --
+        Player2_Bid_New : out std_logic_vector (1 downto 0);
+        Player3_Bid_New : out std_logic_vector (1 downto 0);
+        Player4_Bid_New : out std_logic_vector (1 downto 0);
+
+        Player1_win_type : out std_logic_vector (2 downto 0);
+        Player2_win_type : out std_logic_vector (2 downto 0);
+        Player3_win_type : out std_logic_vector (2 downto 0);
+        Player4_win_type : out std_logic_vector (2 downto 0);
+
+        Player_Turn_New : out std_logic_vector (2 downto 0); -- outputs -> mem based on actions --
+        Receiving_Hand  : out std_logic_vector (2 downto 0); -- pointer to which hand the new card is added to (3 bits for 1, 2, 3, 4, dealer, reserve--
+
+        enable        : out std_logic;
+        bid_enable    : out std_logic;
+        Player1_Broke : out std_logic;
+        Player2_Broke : out std_logic;
+        Player3_Broke : out std_logic;
+        Player4_Broke : out std_logic;
+
+        even_money : out std_logic;
+        insurance  : out std_logic;
+        split      : out std_logic;
+        double     : out std_logic;
+
+        round_end    : out std_logic
+    );
+end controller;
 
 architecture behaviour of controller is
 type controller_state is ( reset_state,
@@ -115,9 +232,7 @@ begin
 				choose_action <= '0';
 				score_screen <= '0';
 
-				global_reset <= '1';
-				start_screen <= '1';
-				draw_screen_type <= "00"; 
+				draw_screen_type <= "10"; 
 				new_state <= player_action;
 
 		request_card <= '0';
@@ -156,7 +271,6 @@ begin
 		even_money_option <= even_money_selectable;
 
 		round_end <= '0';
-		global_reset <= '0';
 
 
 		case state is
@@ -207,10 +321,9 @@ begin
 				choose_action <= '0';
 				score_screen <= '0';
 
-				global_reset <= '1';
 				start_screen <= '1';
 				draw_screen_type <= "00"; 
-				new_state <= player_action;
+				new_state <= game_setup;
 				
 					
 			when game_setup =>    
@@ -225,12 +338,12 @@ begin
 				bid_successful <= '0';
 					
 				if ( start_screen = '0' ) and ( score_screen = '0' ) and ( bids_placed = '1' ) then
-					draw_screen_type <= "10";    ----- 10 tells graphics cursor to track the action menu -----
-					choose_action <= '1';
-				
-
-				elsif ( start_screen = '1' ) then
-					new_state <= player_action;
+				draw_screen_type <= "10";    ----- 10 tells graphics cursor to track the action menu -----
+				choose_action <= '1';
+			
+            elsif ( start_screen = '1' ) then
+				new_state <= player_action;
+				draw_screen_type<= "00";
 
 				elsif ( bids_placed = '0' ) and ( N_Players /= "000" ) and ( start_screen = '0' ) then	 -- bidding screen condition--
 					draw_screen_type <= "01";    ----- 10 tells graphics cursor to track the bidding menu -----
@@ -650,7 +763,7 @@ begin
 								new_state <= game_setup;
 
 						elsif ( unsigned(Player3_Hand_Score) = unsigned(Dealer_Hand_Score) ) then
-								Player3_win_type <= "001";
+								Player3_win_type <= "001";start_screen
 								new_state <= game_setup;
 
 						elsif ( unsigned(Player4_Hand_Score) = unsigned(Dealer_Hand_Score) ) then
@@ -696,7 +809,7 @@ begin
 								Player2_win_type <= "100";
 								new_state <= game_setup;
 							end if;
-
+start_screen
 						elsif ( unsigned(Player3_Hand_Score) > unsigned(Dealer_Hand_Score) ) then
 							if ( Player3_Doubled_Down = '1' ) then
 								Player3_win_type <= "010";
@@ -737,8 +850,9 @@ begin
 				end if;
 		
 			when player_action =>
-						
-				if ( start_screen = '1' ) then                              -- menu for choosing players --
+				if ( start_screen = '1' ) then    
+					draw_screen_type<= "00";
+					-- menu for choosing players --
 					if ( N_Players = "000" ) then
 						if ( switch_left = '1' ) then
 							if ( current_screen_position = "001" ) then     -- if at option 1, left moves to option 4 --
@@ -765,6 +879,8 @@ begin
 								Player3_Inactive <= '1';
 								Player4_Inactive <= '1';
 								enable <= '1';
+								
+						      start_screen <= '0';
 								--new_state <= player_action;
 								new_state <= game_setup;
 
@@ -773,6 +889,8 @@ begin
 								Player3_Inactive <= '1';
 								Player4_Inactive <= '1';
 								enable <= '1';
+								
+				            start_screen <= '1';
 								--new_state <= player_action;
 								--choose_action <= '1';
 								--draw_screen_type <= "01";     --- 01 says draw the bidding box ---
@@ -785,7 +903,8 @@ begin
 								enable <= '1';
 								--new_state <= player_action;
 								new_state <= game_setup;
-	
+
+				start_screen <= '1';
 							else
 								N_Players <= "100";
 								enable <= '1';
@@ -793,7 +912,7 @@ begin
 								new_state <= game_setup;
 							end if;
 						else
-							new_state <= player_action;
+							new_state <= player_action;start_screen
 						end if;
 							
 					else
@@ -1136,8 +1255,7 @@ begin
 							round_end <= '1';                            ---- round end executes a pseudo-reset on memory, RCM checks deck ----
 							new_state <= game_setup;
 
-						else
-							global_reset <= '1';                                ---- reset send to all modules ----
+						else                              ---- reset send to all modules ----
 							new_state <= reset_state;
 						end if;
 					else
@@ -1154,6 +1272,7 @@ begin
 							
 				elsif (second_card_deal = '1') and (random_card = "0000") then
 					--require_card <= '1';
+architecture beh
 					new_state <= pending_card_a;
 							
 				elsif (dealer_card_deal = '1') and (random_card = "0000") then
@@ -1343,3 +1462,4 @@ begin
             end case;
       end process;
 end architecture;
+
